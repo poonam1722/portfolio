68b6b47f4d16e2d86cded5b69de62bf32462db69
poonam1722 <48404211+poonam1722@users.noreply.github.com>
2020-07-25 17:23:46 +0530

    in

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const formrouter = require('./routes/form');
 app.use('/contact', formrouter);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('crm -rf .git/lient/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
